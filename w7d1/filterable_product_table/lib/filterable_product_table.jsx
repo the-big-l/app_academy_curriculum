201883a7f719d4c2b7d001f9af7f7ece21420abf
Leo Salat <leo.salat@gmail.com>
2017-03-04 15:11:31 -0800

    add product table and children components

--- w7d1/filterable_product_table/lib/filterable_product_table.jsx
+++ w7d1/filterable_product_table/lib/filterable_product_table.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
-import SearchBar from 'searchbar';
-import ProductTable from 'product_table';
+import SearchBar from './searchbar';
+import ProductTable from './product_table';
 
 export default class FilterableProductTable extends React.Component {
   render() {
     return (
       <div>
         <SearchBar />
-        {/*<ProductTable products={this.props.products} />*/}
+        <ProductTable products={this.props.products} />
       </div>
     );
   }
--- w7d1/filterable_product_table/lib/main.jsx
+++ w7d1/filterable_product_table/lib/main.jsx
@@ -3,8 +3,15 @@ import ReactDOM from 'react-dom';
 import FilterableProductTable from './filterable_product_table';
 
 document.addEventListener("DOMContentLoaded", () => {
+var PRODUCTS = [
+{category: 'Sports', price: '$49.99', stocked: true, name: 'Football'},
+{category: 'Sports', price: '$9.99', stocked: true, name: 'Baseball'},
+{category: 'Sports', price: '$29.99', stocked: false, name: 'Basketball'},
+{category: 'Electronics', price: '$99.99', stocked: true, name: 'iPod Touch'},
+{category: 'Electronics', price: '$399.99', stocked: false, name: 'iPhone 5'},
+{category: 'Electronics', price: '$199.99', stocked: true, name: 'Nexus 7'}
+];
   const root = document.getElementById("root");
 
-  // ReactDOM.render(<h1>this is a test</h1>, root);
-  ReactDOM.render(<FilterableProductTable />, root);
+  ReactDOM.render(<FilterableProductTable products={PRODUCTS}/>, root);
 });
