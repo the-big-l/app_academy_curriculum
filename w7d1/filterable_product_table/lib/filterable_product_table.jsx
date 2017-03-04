cbd8d91235e1181769d30df0b31ad79ba72f282f
Leo Salat <leo.salat@gmail.com>
2017-03-04 14:51:56 -0800

    add main parent component

---
+++ w7d1/filterable_product_table/lib/filterable_product_table.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class FilterableProductTable extends React.Component {
+  render() {
+    return (
+      <div>
+        <SearchBar />
+        <ProductTable products={this.props.products} />
+      </div>
+    );
+  }
