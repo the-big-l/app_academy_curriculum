13b36460de2e5f8e268202d7efd2695696739b9a
Leo Salat <leo.salat@gmail.com>
2017-03-04 15:00:50 -0800

    add searchbar component

--- w7d1/filterable_product_table/lib/filterable_product_table.jsx
+++ w7d1/filterable_product_table/lib/filterable_product_table.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
+import SearchBar from 'searchbar';
+import ProductTable from 'product_table';
 
 export default class FilterableProductTable extends React.Component {
   render() {
     return (
       <div>
         <SearchBar />
-        <ProductTable products={this.props.products} />
+        {/*<ProductTable products={this.props.products} />*/}
       </div>
     );
   }
+}
