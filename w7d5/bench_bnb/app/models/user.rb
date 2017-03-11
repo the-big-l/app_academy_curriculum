90de143838cf59748897812350dd101550c40a42
Leo Salat <leo.salat@gmail.com>
2017-03-10 16:51:14 -0800

    add auth api

---
+++ w7d5/bench_bnb/app/models/user.rb
@@ -0,0 +1,48 @@
+# == Schema Information
+#
+# Table name: users
+#
+#  id              :integer          not null, primary key
+#  username        :string           not null
+#  session_token   :string           not null
+#  password_digest :string           not null
+#  created_at      :datetime         not null
+#  updated_at      :datetime         not null
+#
+class User < ApplicationRecord
+  attr_reader :password
+
+  validates :username, :session_token, presence: true, uniqueness: true
+  validates :password_digest, presence: true
+  validates :password, length: { minimum: 6, allow_nil: true }
+
+  after_initialize :ensure_token
+
+  def self.generate_token
+    SecureRandom.urlsafe_base64(16)
+  end
+
+  def self.find_by_credentials(username, password)
+    user = User.find_by_username(username)
+    user && user.valid_password?(password) ? user : nil
+  end
+
+  def reset_token!
+    self.session_token = self.class.generate_token
+    self.save
+    self.session_token
+  end
+
+  def ensure_token
+    self.session_token ||= self.class.generate_token
+  end
+
+  def password=(password)
+    @password = password
+    self.password_digest = BCrypt::Password.create(password)
+  end
+
+  def valid_password?(password)
+    BCrypt::Password.new(password_digest).is_password?(password)
+  end
+end
