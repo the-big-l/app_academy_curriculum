e0e6ee19472ce52c1f75bfa11f64b16e017ebd0f
Leo Salat <leo.salat@gmail.com>
2017-03-13 09:55:26 -0700

    add session reducer; add to root reducer

---
+++ w7d5/bench_bnb/frontend/reducers/session_reducer.js
@@ -0,0 +1,23 @@
+import {
+  RECEIVE_CURRENT_USER,
+  RECEIVE_ERRORS } from '../actions/session_actions';
+import merge from 'lodash/merge';
+
+const _nullUser = Object.freeze({
+  currentUser: null,
+  errors: []
+});
+
+const sessionReducer = (state = _nullUser, action) => {
+  Object.freeze(state);
+  switch (action.type) {
+  case RECEIVE_CURRENT_USER;
+    return merge({}, _nullUser, action.currentUser);
+  case RECEIVE_ERRORS;
+    return merge({}, _nullUser, action.errors);
+  default;
+    return state;
+  }
+};
+
+export default sessionReducer;
