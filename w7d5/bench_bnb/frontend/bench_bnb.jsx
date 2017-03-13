77a371a25e60fa6ea747ee9085ce32ca6b5c1240
Leo Salat <leo.salat@gmail.com>
2017-03-13 10:02:19 -0700

    add store to window for testing

--- w7d5/bench_bnb/frontend/bench_bnb.jsx
+++ w7d5/bench_bnb/frontend/bench_bnb.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import configureStore from './store/store';
 
 // Testing
 import * as Util from './util/session_api_util';
@@ -11,6 +12,8 @@ document.addEventListener('DOMContentLoaded', () => {
     window.login = Util.login;
     window.signup = Util.signup;
     window.logout = Util.logout;
+    window.store = configureStore();
+
 
     ReactDOM.render(<h1>Welcome to BenchBnB</h1>, root);
 });
