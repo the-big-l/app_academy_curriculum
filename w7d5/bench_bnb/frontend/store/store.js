4898f443f56299e7825801862fa9a1038b13f656
Leo Salat <leo.salat@gmail.com>
2017-03-13 10:05:24 -0700

    fix type creatStore > createStore

--- w7d5/bench_bnb/frontend/store/store.js
+++ w7d5/bench_bnb/frontend/store/store.js
@@ -3,7 +3,7 @@ import thunk from 'redux-thunk';
 import rootReducer from '../reducers/root_reducer';
 
 const configureStore = preloadedState => (
-  creatStore(
+  createStore(
     rootReducer,
     preloadedState,
     applyMiddleware(thunk)
