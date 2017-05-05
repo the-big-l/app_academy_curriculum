7daa2761ed36f78f1aa656f0029c7cc79918ec63
Leo Salat <leo.salat@gmail.com>
2017-05-04 17:43:13 -0700

    add validation/assoc tests

--- w15/benchbnb/spec/models/user_spec.rb
+++ w15/benchbnb/spec/models/user_spec.rb
@@ -1,5 +1,25 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validations' do
+    before(:each) do
+      User.create!(username: 'breakfast', password: 'password')
+    end
+    it { should validate_presence_of(:username) }
+    it { should validate_presence_of(:password_digest) }
+    it { should validate_presence_of(:session_token) }
+
+    it { should validate_uniqueness_of(:username) }
+
+    it do
+      should validate_length_of(:password).
+        is_at_least(6).
+        on(:create)
+    end
+  end
+
+  describe 'associations' do
+    it { should have_many(:favorites) }
+    it { should have_many(:favorite_benches) }
+  end
 end
