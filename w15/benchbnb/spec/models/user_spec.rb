f9cded9b4e8b47fb997b64034c8dfb1cf1463174
Leo Salat <leo.salat@gmail.com>
2017-05-17 18:09:40 -0700

    move let statement outside of validations block

--- w15/benchbnb/spec/models/user_spec.rb
+++ w15/benchbnb/spec/models/user_spec.rb
@@ -1,11 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  describe 'validations' do
-    let!(:user) do
-      User.create!(username: 'breakfast', password: 'password')
-    end
+  let!(:user) do
+    User.create!(username: 'breakfast', password: 'password')
+  end
 
+  describe 'validations' do
     it { should validate_presence_of(:username) }
     it { should validate_presence_of(:password_digest) }
     it { should validate_presence_of(:session_token) }
@@ -22,5 +22,17 @@ RSpec.describe User, type: :model do
   describe 'associations' do
     it { should have_many(:favorites) }
     it { should have_many(:favorite_benches) }
+  end
+
+  describe 'model_methods' do
+    describe '.find_by_credentials' do
+      context 'when given correct credentials' do
+        it 'should find the right user'
+      end
+
+      context 'when given incorrect credentials' do
+        it 'should return nil'
+      end
+    end
   end
 end
